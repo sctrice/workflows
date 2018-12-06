1654203ab293ec1acb9254302bb0c2ee195d29bb
Steven Trice <sctrice@vpas.vt.edu>
2018-12-06 16:36:16 -0500

    added coffeescript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+
+var coffeeSource = ['components/coffee/tagline.coffee'];
 
 gulp.task('log', function(){
     gutil.log('workflows are awesome');
+});
+
+gulp.task('coffee', function() {
+    gulp.src(coffeeSource)
+        .pipe(coffee({bare: true})
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'));
 });
